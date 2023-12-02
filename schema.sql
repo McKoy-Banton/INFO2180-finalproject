0b761d1b6784022d63da3d85e85c3cb5ff24f281
Mickoy Banton <mickoybanton11@gmail.com>
2023-12-02 02:07:20 -0500

    Program to use database.

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(35) NOT NULL DEFAULT '',
   `lastname` varchar(35) NOT NULL DEFAULT '',
-  `password` varchar(20) NOT NULL,
+  `password` varchar(256) NOT NULL,
   `email` varchar(50) NOT NULL DEFAULT '',
   `role` varchar(20) NOT NULL DEFAULT '',
   `create_at` DATETIME,
@@ -41,5 +41,5 @@ CREATE TABLE `notes` (
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8mb4;
 
-SET @hashed_password = SHA2('password123', 256);
+SET @hashed_password = SHA2('password123', 256); 
 INSERT INTO users (email, password, role, create_at) VALUES ('admin@project2.com', @hashed_password, 'admin', NOW());
