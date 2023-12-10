0341f2ffd60a52c3d2b9148a3ae1d81ad3a91c27
Mickoy Banton <mickoybanton11@gmail.com>
2023-12-10 13:07:59 -0500

    Added a break between user and logout.

--- viewcontact.php
+++ viewcontact.php
@@ -81,7 +81,7 @@ function convertTimeFormat(){
 
     <a href="dashboard.php" class="currentPage"><li><i class="material-icons">home</i>Home</li></a>
     <a href="create-contact.php"><li><i class="material-icons">account_circle</i>New Contact</li></a>
-    <a href="view_users.php"><li><i class="material-icons">people_outline</i>Users</li></a>
+    <a href="view_users.php"><li><i class="material-icons">people_outline</i>Users</li> </a> <br>
     <hr>
     <a href="logout.php"><li><i class="material-icons">exit_to_app</i>Logout</li></a>
             
